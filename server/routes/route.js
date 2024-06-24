79055d8319490ad0db8960b1560df4fdaa420aee
N1ckool <104719138+Div7195@users.noreply.github.com>
2024-06-24 19:14:40 +0530

    minor change

--- server/routes/route.js
+++ server/routes/route.js
@@ -8,18 +8,18 @@ import checkAuthentication from '../middleware/checkAuth.js';
 
 const router = express.Router();
 
-router.post('/addCustomer',checkAuthentication, addCustomerController);
-router.get('/getCustomers',checkAuthentication, getCustomersController);
-router.post('/addOrder',checkAuthentication, addNewOrderController);
-router.get('/getOrders',checkAuthentication, getOrdersController);
-router.post('/getAudienceSize',checkAuthentication, checkAudienceSizeController);
-router.post('/addAudience',checkAuthentication, saveAudienceController);
-router.post('/sendEmails',checkAuthentication, sendEmailsController);
-router.post('/sendAnEmail',checkAuthentication, sendAnEmailController)
-router.post('/updateDeliveryReceipt',checkAuthentication, updateDeliveryReceiptController);
-router.get('/getAllCampaigns',checkAuthentication, getCampaignsController);
-router.get('/getAllAudiences',checkAuthentication, getAllAudiencesController);
-router.get('/getSingleAudience/:audienceId',checkAuthentication, getAllAudiencesController);
-router.post('/getDeliveryReceipts',checkAuthentication, deliveryReceiptController);
+router.post('/addCustomer', addCustomerController);
+router.get('/getCustomers', getCustomersController);
+router.post('/addOrder', addNewOrderController);
+router.get('/getOrders', getOrdersController);
+router.post('/getAudienceSize', checkAudienceSizeController);
+router.post('/addAudience', saveAudienceController);
+router.post('/sendEmails', sendEmailsController);
+router.post('/sendAnEmail', sendAnEmailController)
+router.post('/updateDeliveryReceipt', updateDeliveryReceiptController);
+router.get('/getAllCampaigns', getCampaignsController);
+router.get('/getAllAudiences', getAllAudiencesController);
+router.get('/getSingleAudience/:audienceId', getAllAudiencesController);
+router.post('/getDeliveryReceipts', deliveryReceiptController);
 
 export default router;
